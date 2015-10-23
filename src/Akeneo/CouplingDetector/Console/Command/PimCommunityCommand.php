23bfdef4deddc0caca01c9a21a323a2095bf2755
Nicolas Dupont <nicolas@akeneo.com>
2015-10-23 18:39:50 +0200

    Add different output mode for the command

--- src/Akeneo/CouplingDetector/Console/Command/PimCommunityCommand.php
+++ src/Akeneo/CouplingDetector/Console/Command/PimCommunityCommand.php
@@ -2,6 +2,7 @@
 
 namespace Akeneo\CouplingDetector\Console\Command;
 
+use Akeneo\CouplingDetector\Coupling\UseViolations;
 use Akeneo\CouplingDetector\Detector;
 use Akeneo\CouplingDetector\Coupling\UseViolationsFilter;
 use Akeneo\CouplingDetector\FilesReader;
@@ -30,6 +31,11 @@ class PimCommunityCommand extends Command
             ->setName('pim-community-dev')
             ->setDefinition(
                 array(
+                    new InputOption(
+                        'output',
+                        '',
+                        InputOption::VALUE_REQUIRED, 'Output mode, "default", "count", "none"', 'default'
+                    ),
                     new InputOption('strict', '', InputOption::VALUE_NONE, 'Apply strict rules without legacy exceptions'),
                 )
             )
@@ -41,24 +47,18 @@ class PimCommunityCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        // check the project path
-        $binPath = getcwd();
-        $composerPath = $binPath.'/composer.json';
-        $errorMessage = sprintf('You must launch the command from the pim-community-dev repository, not from "%s"', $binPath).PHP_EOL;
-        if (!file_exists($composerPath)) {
-            fwrite(STDERR, $errorMessage);
-            exit(1);
-        } else {
-            $composerContent = file_get_contents($composerPath);
-            if (false === strpos($composerContent, '"name": "akeneo/pim-community-dev"')) {
-                fwrite(STDERR, $errorMessage);
-                exit(1);
-            }
-        }
-        $path = $binPath.'/src/';
+        $path = $this->getProjectPath();
         $strictMode = $input->getOption('strict');
+        $displayMode = $input->getOption('output');
 
-        $output->writeln(sprintf('<info> Detect coupling violations (strict mode %s)</info>', $strictMode ? 'enabled' : 'disabled'));
+        if ('none' !== $displayMode) {
+            $output->writeln(
+                sprintf(
+                    '<info> Detect coupling violations (strict mode %s)</info>',
+                    $strictMode ? 'enabled' : 'disabled'
+                )
+            );
+        }
 
         $rules = [
             new UseViolationRule('Akeneo\Component', ['Pim', 'PimEnterprise', 'Bundle']),
@@ -86,6 +86,41 @@ class PimCommunityCommand extends Command
             // $violations = $useViolationsFilter->filter($violations);
             // TODO: to be refactored, does not work with new reading strategy
         }
+
+        if ('default' === $displayMode) {
+            $this->displayStandardViolations($output, $violations);
+
+        } elseif ('count' === $displayMode) {
+            $this->displayCounterViolations($output, $violations);
+        }
+
+        return count($violations->getFullQualifiedClassNameViolations()) > 0;
+    }
+
+    /**
+     * @param OutputInterface $output
+     * @param UseViolations   $violations
+     */
+    protected function displayStandardViolations(OutputInterface $output, UseViolations $violations)
+    {
+        $violations = $violations->getFullQualifiedClassNameViolations();
+        $totalCount = 0;
+        foreach ($violations as $className => $violationUses) {
+            $output->writeln(sprintf('<info>Violations in %s</info>', $className));
+            foreach ($violationUses as $use) {
+                $output->writeln(sprintf('<info> - use %s</info>', $use));
+            }
+            $totalCount += count($violationUses);
+        }
+        $output->writeln(sprintf('<info>Total coupling issues %s</info>', $totalCount));
+    }
+
+    /**
+     * @param OutputInterface $output
+     * @param UseViolations   $violations
+     */
+    protected function displayCounterViolations(OutputInterface $output, UseViolations $violations)
+    {
         $forbiddenUseCounter = $violations->getSortedForbiddenUsesCounters();
         $totalCount = 0;
         foreach ($forbiddenUseCounter as $fullName => $count) {
@@ -93,7 +128,30 @@ class PimCommunityCommand extends Command
             $totalCount += $count;
         }
         $output->writeln(sprintf('<info>Total coupling issues %s</info>', $totalCount));
+    }
 
-        return $totalCount;
+    /**
+     * @return string
+     */
+    protected function getProjectPath()
+    {
+        $binPath = getcwd();
+        $composerPath = $binPath.'/composer.json';
+        $errorMessage = sprintf(
+                'You must launch the command from the pim-community-dev repository, not from "%s"',
+                $binPath
+            ).PHP_EOL;
+        if (!file_exists($composerPath)) {
+            fwrite(STDERR, $errorMessage);
+            exit(1);
+        } else {
+            $composerContent = file_get_contents($composerPath);
+            if (false === strpos($composerContent, '"name": "akeneo/pim-community-dev"')) {
+                fwrite(STDERR, $errorMessage);
+                exit(1);
+            }
+        }
+
+        return $binPath.'/src/';
     }
 }
