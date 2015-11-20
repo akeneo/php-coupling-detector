43e9508663e651805f9ebc1b86a66f214c67227f
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2015-11-20 18:55:27 +0100

    Introduction of RuleChecker

---
+++ spec/Akeneo/CouplingDetector/RuleCheckerSpec.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace spec\Akeneo\CouplingDetector;
+
+use Akeneo\CouplingDetector\Data\NodeInterface;
+use Akeneo\CouplingDetector\Data\RuleInterface;
+use Akeneo\CouplingDetector\Data\ViolationInterface;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class RuleCheckerSpec extends ObjectBehavior
+{
+    function it_matches_a_node(RuleInterface $rule, NodeInterface $node)
+    {
+        $rule->getSubject()->willReturn('foo\bar');
+        $node->getSubject()->willReturn('foo\bar\baz');
+        $this->match($rule, $node)->shouldReturn(true);
+
+        $rule->getSubject()->willReturn('foo\bar');
+        $node->getSubject()->willReturn('blu\bla\blo');
+        $this->match($rule, $node)->shouldReturn(false);
+    }
+
+    function it_checks_a_valid_node_with_forbidden_rule(RuleInterface $rule, NodeInterface $node)
+    {
+        $rule->getSubject()->willReturn('foo\bar');
+        $rule->getRequirements()->willReturn(['blu', 'bla', 'bli']);
+        $rule->getType()->willReturn(RuleInterface::TYPE_FORBIDDEN);
+        $node->getSubject()->willReturn('foo\bar\baz');
+        $node->getTokens()->willReturn(['blo', 'bly']);
+
+        $this->check($rule, $node)->shouldReturn(null);
+    }
+
+    function it_checks_an_invalid_node_with_forbidden_rule(
+        RuleInterface $rule,
+        NodeInterface $node,
+        ViolationInterface $violation
+    ) {
+        $rule->getSubject()->willReturn('foo\bar');
+        $rule->getRequirements()->willReturn(['blu', 'bla', 'bli']);
+        $rule->getType()->willReturn(RuleInterface::TYPE_FORBIDDEN);
+        $node->getSubject()->willReturn('foo\bar\baz');
+        $node->getTokens()->willReturn(['blu', 'bla', 'blo', 'bly']);
+
+        $violation->getNode()->willReturn($node);
+        $violation->getRule()->willReturn($rule);
+        $violation->getType()->willReturn(ViolationInterface::TYPE_ERROR);
+        $violation->getTokenViolations()->willReturn(['blu', 'bla']);
+
+        $this->check($rule, $node)->shouldBeLikeExpectedViolation($violation);
+    }
+
+    function it_checks_a_valid_node_with_discouraged_rule(RuleInterface $rule, NodeInterface $node)
+    {
+        $rule->getSubject()->willReturn('foo\bar');
+        $rule->getRequirements()->willReturn(['blu', 'bla', 'bli']);
+        $rule->getType()->willReturn(RuleInterface::TYPE_DISCOURAGED);
+        $node->getSubject()->willReturn('foo\bar\baz');
+        $node->getTokens()->willReturn(['blo', 'bly']);
+
+        $this->check($rule, $node)->shouldReturn(null);
+    }
+
+    function it_checks_an_invalid_node_with_discouraged_rule(
+        RuleInterface $rule,
+        NodeInterface $node,
+        ViolationInterface $violation
+    ) {
+        $rule->getSubject()->willReturn('foo\bar');
+        $rule->getRequirements()->willReturn(['blu', 'bla', 'bli']);
+        $rule->getType()->willReturn(RuleInterface::TYPE_DISCOURAGED);
+        $node->getSubject()->willReturn('foo\bar\baz');
+        $node->getTokens()->willReturn(['blu', 'bla', 'blo', 'bly']);
+
+        $violation->getNode()->willReturn($node);
+        $violation->getRule()->willReturn($rule);
+        $violation->getType()->willReturn(ViolationInterface::TYPE_WARNING);
+        $violation->getTokenViolations()->willReturn(['blu', 'bla']);
+
+        $this->check($rule, $node)->shouldBeLikeExpectedViolation($violation);
+    }
+
+    function it_checks_a_valid_node_with_only_rule(RuleInterface $rule, NodeInterface $node)
+    {
+        $rule->getSubject()->willReturn('foo\bar');
+        $rule->getRequirements()->willReturn(['blu', 'bla', 'bli']);
+        $rule->getType()->willReturn(RuleInterface::TYPE_ONLY);
+        $node->getSubject()->willReturn('foo\bar\baz');
+        $node->getTokens()->willReturn(['blu', 'bla']);
+
+        $this->check($rule, $node)->shouldReturn(null);
+    }
+
+    function it_checks_an_invalid_node_with_only_rule(
+        RuleInterface $rule,
+        NodeInterface $node,
+        ViolationInterface $violation
+    ) {
+        $rule->getSubject()->willReturn('foo\bar');
+        $rule->getRequirements()->willReturn(['blu', 'bla', 'bli']);
+        $rule->getType()->willReturn(RuleInterface::TYPE_ONLY);
+        $node->getSubject()->willReturn('foo\bar\baz');
+        $node->getTokens()->willReturn(['blu', 'bla', 'blo', 'bly']);
+
+        $violation->getNode()->willReturn($node);
+        $violation->getRule()->willReturn($rule);
+        $violation->getType()->willReturn(ViolationInterface::TYPE_ERROR);
+        $violation->getTokenViolations()->willReturn(['blo', 'bly']);
+
+        $this->check($rule, $node)->shouldBeLikeExpectedViolation($violation);
+    }
+
+    public function getMatchers()
+    {
+        return [
+            'beLikeExpectedViolation' => function ($subject, $expected) {
+                return
+                    $subject->getNode() === $expected->getNode() &&
+                    $subject->getRule() === $expected->getRule() &&
+                    $subject->getTokenViolations() === $expected->getTokenViolations() &&
+                    $subject->getType() === $expected->getType();
+            },
+        ];
+    }
+}
---
+++ src/Akeneo/CouplingDetector/RuleChecker.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Akeneo\CouplingDetector;
+
+use Akeneo\CouplingDetector\Data\NodeInterface;
+use Akeneo\CouplingDetector\Data\RuleInterface;
+use Akeneo\CouplingDetector\Data\Violation;
+use Akeneo\CouplingDetector\Data\ViolationInterface;
+
+/**
+ * Check if a node respects or matches a rule.
+ *
+ * There 3 types or rules at the moment:
+ *   -"forbidden": A node respects such a rule if no rule token is present in the node. In case the node does not
+ *                 respect this rule, an error violation will be sent.
+ *   -"discouraged": A node respects such a rule if no rule token is present in the node. In case the node does not
+ *                  respect this rule, a warning violation will be sent.
+ *   -"only": A node respects such a rule if the node contains only tokens defined in the rule. In case the node
+ *            does not respect this rule, an error violation will be sent.
+ *
+ * @author  Julien Janvier <j.janvier@gmail.com>
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+class RuleChecker
+{
+    /**
+     * Does a node matches a rule?
+     *
+     * @param RuleInterface $rule
+     * @param NodeInterface $node
+     *
+     * @return bool
+     */
+    public function match(RuleInterface $rule, NodeInterface $node)
+    {
+        if (false !== strpos($node->getSubject(), $rule->getSubject())) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Checks if a node respect a rule.
+     *
+     * @param RuleInterface $rule
+     * @param NodeInterface $node
+     *
+     * @return Violation|null
+     */
+    public function check(RuleInterface $rule, NodeInterface $node)
+    {
+        if (!$this->match($rule, $node)) {
+            return null;
+        }
+
+        switch ($rule->getType()) {
+            case RuleInterface::TYPE_FORBIDDEN:
+            case RuleInterface::TYPE_DISCOURAGED:
+                $violation = $this->checkForbiddenOrDiscouragedRule($rule, $node);
+                break;
+            case RuleInterface::TYPE_ONLY:
+                $violation = $this->checkOnlyRule($rule, $node);
+                break;
+            default:
+                throw new \RuntimeException(sprintf('Unknown rule type "%s".', $rule->getType()));
+        }
+
+        return $violation;
+    }
+
+    /**
+     * Checks if a node respects a "forbidden" or "discouraged" rule.
+     * A node respects such a rule if no rule token is present in the node.
+     *
+     * @param RuleInterface $rule
+     * @param NodeInterface $node
+     *
+     * @return Violation|null
+     */
+    private function checkForbiddenOrDiscouragedRule(RuleInterface $rule, NodeInterface $node)
+    {
+        $errors = [];
+
+        foreach ($node->getTokens() as $token) {
+            foreach ($rule->getRequirements() as $req) {
+                if (strpos($token, $req) !== false && !in_array($token, $errors)) {
+                    $errors[] = $token;
+                }
+            }
+         }
+
+        if (count($errors)) {
+            $type = $rule->getType() === RuleInterface::TYPE_FORBIDDEN ?
+                    ViolationInterface::TYPE_ERROR :
+                    ViolationInterface::TYPE_WARNING
+            ;
+
+            return new Violation($node, $rule, $errors, $type);
+        }
+
+        return null;
+    }
+
+    /**
+     * Checks if a node respects a "forbidden" or "discouraged" rule.
+     * A node respects such a rule if the node contains only tokens defined in the rule.
+     *
+     * @param RuleInterface $rule
+     * @param NodeInterface $node
+     *
+     * @return Violation|null
+     */
+    private function checkOnlyRule(RuleInterface $rule, NodeInterface $node)
+    {
+        $errors = [];
+
+        foreach ($node->getTokens() as $token) {
+            $fitRuleRequirements = false;
+            foreach ($rule->getRequirements() as $req) {
+                if (false !== strpos($token, $req)) {
+                    $fitRuleRequirements = true;
+                }
+            }
+            if (!$fitRuleRequirements && !in_array($token, $errors)) {
+                $errors[] = $token;
+            }
+        }
+
+        if (count($errors)) {
+            return new Violation($node, $rule, $errors, ViolationInterface::TYPE_ERROR);
+        }
+
+        return null;
+    }
+}
